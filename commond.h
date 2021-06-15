9504d100f49182bd839ac82c4e2d1803fa8ea93c
caloja1014 <caloja@espol.edu.ec>
2021-06-15 02:12:12 -0500

    Creation of headers for the connections

---
+++ commond.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 1024
+
+
+int open_listenfd(char *port);
+
+
+int open_clientfd(char *hostname, char *port);
+
+
+void connection_error(int connfd);
+
+#endif
