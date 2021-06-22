d85880ae440979538a13b475d9ec97816c23ebb1
caloja1014 <caloja@espol.edu.ec>
2021-06-21 19:37:12 -0500

    Makefile and gitignore

---
+++ Makefile
@@ -0,0 +1,28 @@
+DEPS = common.h
+CFLAGS= -Wall -c
+
+.PHONY: all debug sanitize clean
+
+all: framework sensor
+
+framework: opprocessors.o common.o $(DEPS) threadpool.h
+	gcc -o $@ opprocessors.o common.o -L. -lpthread $(DFLAGS) threadpool.h
+
+sensor: sensor.o common.o $(DEPS)
+	gcc -o $@ sensor.o common.o $(DFLAGS)
+
+
+%.o: %.c $(DEPS) threadpool.h
+	gcc $(CFLAGS) $< $(DFLAGS)
+
+# Compila usando la opción -g para facilitar la depuración con gdb.
+debug: DFLAGS = -g
+debug: clean all
+
+# Compila habilitando la herramienta AddressSanitizer para
+# facilitar la depuración en tiempo de ejecución.
+sanitize: DFLAGS = -fsanitize=address,undefined
+sanitize: clean all
+
+clean:
+	rm -rf sensor framework *.o
--- opprocessors.c
+++ opprocessors.c
@@ -2,9 +2,107 @@
 #include <limits.h>
 #include <sys/wait.h>
 
-#include <pthread.h>
 #include <syslog.h>
 #include <fcntl.h>
+#include "threadpool.h"
+#include "common.h"
+#define MAXLINE 1024
+void *atender_cliente(void *connfd);
 
-#include <common.h>
+void print_help(char *command)
+{
 
+    printf("Op Procesor\n");
+    printf("Use:\n %s <puerto>", command);
+    printf(" %s -h\n", command);
+    printf("Opciones:\n");
+    printf(" -h\t\t\tHelp, show this message\n");
+    printf(" -d\t\t\tRun in modo-debug in other case it will run in modo-realtime");
+}
+int dflag;
+int main(int argc, char const **argv)
+{
+    int opt, index;
+
+    //Sockets
+    int listenfd, *connfd;
+    unsigned int clientlen;
+    //Direcciones y puertos
+    struct sockaddr_in clientaddr;
+    struct hostent *hp;
+    char *haddrp, *port;
+
+    while ((opt = getopt(argc, argv, "hd")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            print_help(argv[0]);
+            return 0;
+        case 'd':
+            dflag = 1;
+            break;
+        default:
+            fprintf(stderr, "uso: %s <puerto>\n", argv[0]);
+            fprintf(stderr, "     %s -h\n", argv[0]);
+            fprintf(stderr, "     %s -d\n", argv[0]);
+            return -1;
+        }
+    }
+    for (index = optind; index < argc; index++)
+        port = argv[index];
+    if (argv == NULL)
+    {
+        fprintf(stderr, "uso: %s <puerto>\n", argv[0]);
+        fprintf(stderr, "     %s -h\n", argv[0]);
+        fprintf(stderr, "     %s -d\n", argv[0]);
+        return 1;
+    }
+
+    int port_n = atoi(port);
+	if(port_n <= 0 || port_n > USHRT_MAX){
+		fprintf(stderr, "Puerto: %s invalido. Ingrese un número entre 1 y %d.\n", port, USHRT_MAX);
+		return 1;
+	}
+    listenfd = open_listenfd(port);
+    if(listenfd < 0)
+		connection_error(listenfd);
+
+    
+    tpool_t *tm;
+    size_t num_threads=pcthread_get_num_procs();
+    tm=tpool_create(num_threads);
+
+    while (1)
+    {
+        clientlen = sizeof(clientaddr);
+		connfd = malloc(sizeof(int));
+        *connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+        tpool_add_work(tm,atender_cliente,connfd);
+
+    }
+    
+
+    tpool_wait(tm);
+
+    tpool_destroy(tm);
+    //printf("%d\n",pcthread_get_num_procs());
+    return 0;
+}
+
+void *atender_cliente(void *connfd){
+    int fd_conn = *((int*)connfd);
+    int n, status;
+	char buf[MAXLINE] = {0};
+
+    while (1)
+    {
+        n = read(connfd, buf, MAXLINE);
+		if(n <= 0)
+			return;
+
+		printf("Recibido: %s", buf);
+        memset(buf, 0, MAXLINE); 
+    }
+    
+}
